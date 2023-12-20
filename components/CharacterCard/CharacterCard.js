b74b2bd2396b2793b25c919a133530e4408972ba
somaSeb <133811635+somaSeb@users.noreply.github.com>
2023-12-20 23:06:37 +0100

    takesElementsAsShould

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,32 +1,53 @@
 console.clear();
 export function Card(characterData) {
-  const card = document.createElement("article"); // Create a new article Element
+  const card = document.createElement("div"); // Create a new article Element
   card.classList.add("card"); // Add the class 'card'
 
   // Fill the article element with content.
-  const img = document.createElement("img");
-  img.setAttribute("src", characterData.image);
-  img.setAttribute("alt", "na");
-  img.setAttribute("height", "200");
-  img.setAttribute("width", "200");
-  img.textContent = characterData.image;
-  card.append(img);
 
-  const heading = document.createElement("h2");
-  heading.textContent = characterData.name;
-  card.append(heading);
+  const cardContainer = document.createElement("ul");
+  cardContainer.classList.add("card-container");
+  cardContainer.setAttribute("data-js", "card-container");
+  cardContainer.textContent = "test";
+  card.append(cardContainer);
 
-  const eyeColor = document.createElement("p");
-  eyeColor.textContent = `Status: ${characterData.status}`;
-  card.append(eyeColor);
+  const cardLi = document.createElement("li");
+  cardLi.classList.add("card");
+  card.append(cardLi);
 
-  const birthYear = document.createElement("p");
-  birthYear.textContent = `Type: ${characterData.species}`;
-  card.append(birthYear);
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("card__image-container");
+  card.append(imageContainer);
 
-  const occurences = document.createElement("p");
-  occurences.textContent = `Occurrences: ${characterData.episode.length}`;
-  card.append(occurences);
+  const image = document.createElement("img");
+  image.classList.add("card__image");
+  image.setAttribute("src", `${characterData.image}`);
+  image.setAttribute("alt", "Rick Sanchez");
+  card.append(image);
+
+  // const img = document.createElement("img");
+  // img.setAttribute("src", characterData.image);
+  // img.setAttribute("alt", "na");
+  // img.setAttribute("height", "200");
+  // img.setAttribute("width", "200");
+  // img.textContent = characterData.image;
+  // card.append(img);
+
+  // const heading = document.createElement("h2");
+  // heading.textContent = characterData.name;
+  // card.append(heading);
+
+  // const eyeColor = document.createElement("p");
+  // eyeColor.textContent = `Status: ${characterData.status}`;
+  // card.append(eyeColor);
+
+  // const birthYear = document.createElement("p");
+  // birthYear.textContent = `Type: ${characterData.species}`;
+  // card.append(birthYear);
+
+  // const occurences = document.createElement("p");
+  // occurences.textContent = `Occurrences: ${characterData.episode.length}`;
+  // card.append(occurences);
 
   return card;
 }
@@ -71,25 +92,4 @@ export function Card(characterData) {
 //   card.append(birthYear);
 
 //   return card;
-// }
-
-//   const cardContainer = document.createElement("ul");
-//   cardContainer.classList.add("card-container");
-//   cardContainer.setAttribute("data-js", "card-container");
-//   document.body.main.append(ul);
-//   cardContainer.textContent = "test";
-
-//   const card = document.createElement("li");
-//   card.classList.add("card");
-//   document.body.main.ul.append(li);
-
-//   const imageContainer = document.createElement("div");
-//   imageContainer.classList.add("card__image-container");
-//   document.body.main.ul.li.append(div);
-
-//   const image = document.createElement("img");
-//   image.classList.add("card__image");
-//   image.setAttribute("src", `${characterData.results.image}`);
-//   image.setAttribute("alt", "Rick Sanchez");
-//   document.body.main.ul.li.div.append(img);
 // }
